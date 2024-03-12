362de64b18e309b21676a40072ce62a4dd81bf92
SreejaVattikonda <157474894+SreejaVattikonda@users.noreply.github.com>
2024-03-12 14:10:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Students from "./components/StudentList";
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: '#6969' }}>
+      <div className="container-fluid" >
+        <Students />
+      </div>
+      </div>
+    
+  );
+}
+
+export default App;
+
